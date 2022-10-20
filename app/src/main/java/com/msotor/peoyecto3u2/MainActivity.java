992df7648e901f3bd51d2f2634b3ba0fd4f8dbea
Ariel Halty <arielhalty@hotmail.com>
2022-10-20 08:48:52 -0300

    proyecto datos persistentes

--- app/src/main/java/com/msotor/peoyecto3u2/MainActivity.java
+++ app/src/main/java/com/msotor/peoyecto3u2/MainActivity.java
@@ -2,13 +2,34 @@ package com.msotor.peoyecto3u2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.et_mail);
+
+        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        et1.setText(preferences.getString("mail", ""));
+
+    }
+    //Método para el botón guardar.
+
+    public void Guardar (View view){
+        SharedPreferences preferencias = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferencias.edit();
+        editor.putString("mail", et1.getText().toString());
+        editor.commit();
+        finish();
     }
 }
