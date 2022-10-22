4ca894d8bb15582b9ee252860df3e55f85e99259
jesuscarrillo02 <j.carrillo19@alumnos.santotomas.cl>
2022-10-22 12:08:19 -0400

    Datos Persistentes terminado

--- app/src/main/java/com/msotor/peoyecto3u2/MainActivity.java
+++ app/src/main/java/com/msotor/peoyecto3u2/MainActivity.java
@@ -2,13 +2,34 @@ package com.msotor.peoyecto3u2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private Button bt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.txt_email);
+
+        SharedPreferences preferences  = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        et1.setText(preferences.getString("mail",""));
+    }
+
+    public void Guardar(View view) {
+        SharedPreferences preferencias  = getSharedPreferences("datos", Context.MODE_PRIVATE);
+        SharedPreferences.Editor Obj_editor = preferencias.edit();
+        Obj_editor.putString("mail", et1.getText().toString());
+        Obj_editor.commit();
+        finish();
     }
 }
---
+++ app/src/main/java/com/msotor/peoyecto3u2/base_de_datos.java
@@ -0,0 +1,14 @@
+package com.msotor.peoyecto3u2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class base_de_datos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_de_datos);
+    }
+}
