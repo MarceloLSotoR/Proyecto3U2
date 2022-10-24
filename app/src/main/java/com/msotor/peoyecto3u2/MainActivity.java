96f632d773a1bbb098058a0e7189e969f0b6b84e
FrancoTorresBarrera <f.torres66@alumnos.santotomas.cl>
2022-10-24 19:25:38 -0300

    Base de datos

--- app/src/main/java/com/msotor/peoyecto3u2/MainActivity.java
+++ app/src/main/java/com/msotor/peoyecto3u2/MainActivity.java
@@ -2,33 +2,84 @@ package com.msotor.peoyecto3u2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et1;
+    private EditText et_codigo, et_descripcion, et_precio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1=(EditText)findViewById(R.id.txt_mail);
-
-        SharedPreferences preferences = getSharedPreferences("datos", Context.MODE_PRIVATE);
-        et1.setText(preferences.getString("mail",""));
+        et_codigo = (EditText)findViewById(R.id.txt_codigo);
+        et_descripcion = (EditText)findViewById(R.id.txt_descripcion);
+        et_precio = (EditText)findViewById(R.id.txt_precio);
     }
 
-    public void Guardar(View view){
-        SharedPreferences preferences = getSharedPreferences("datos",Context.MODE_PRIVATE);
-        SharedPreferences.Editor Obj_editor = preferences.edit();
-        Obj_editor.putString("mail",et1.getText().toString());
-        Obj_editor.commit();
-        finish();
+    public void Registrar(View view){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion",null,1);
+        SQLiteDatabase BaseDeDatos = admin.getWritableDatabase();
+
+        String codigo = et_codigo.getText().toString();
+        String descripcion = et_descripcion.getText().toString();
+        String precio = et_precio.getText().toString();
+
+        if(!codigo.isEmpty() && !descripcion.isEmpty() && !precio.isEmpty()){
+            ContentValues registro = new ContentValues();
+
+            registro.put("codigo",codigo);
+            registro.put("descripcion",descripcion);
+            registro.put("precio",precio);
+
+            BaseDeDatos.insert( "articulos", null, registro);
+            BaseDeDatos.close();
+            et_codigo.setText("");
+            et_descripcion.setText("");
+            et_precio.setText("");
+
+            Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+
+        }else{
+
+            Toast.makeText(this, "Debes llenar todos los campos", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    //Metodo para consultar articulo
+
+    public void Buscar(View view){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion", null, 1);
+        SQLiteDatabase BaseDeDatabase = admin.getWritableDatabase();
+
+        String codigo = et_codigo.getText().toString();
+
+        if(!codigo.isEmpty()){
+            Cursor fila = BaseDeDatabase.rawQuery
+                    ("select descripcion, precio from articulos where codigo =" + codigo, null);
+
+            if(fila.moveToFirst()){
+                et_descripcion.setText(fila.getString(0));
+                et_precio.setText(fila.getString(1));
+                BaseDeDatabase.close();
+            }else{
+                Toast.makeText(this, "No existe el articulo", Toast.LENGTH_SHORT).show();
+                BaseDeDatabase.close();
+            }
+
+        }else{
+            Toast.makeText(this, "Debes introducir el codigo del articulo", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
